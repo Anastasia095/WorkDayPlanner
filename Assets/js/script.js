eb4ff9c6ea6ba2e7a6c57a38cc5b5fe7f6040cf5
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-07-03 20:21:21 -0400

    fixed time-table layout

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,3 +6,12 @@ console.log(timeFormat);
 
 CurrentDay.innerHTML = timeFormat;
 
+
+for(var i = 0; i < document.getElementsByClassName("saveBtn").length; i++) {
+    document.getElementsByClassName("saveBtn")[i].addEventListener('click',function(){
+        console.log("save btn test");
+        var userInput = document.getElementById("col-10").value;
+        localStorage.setItem("userInput", userInput);
+
+    });
+}
